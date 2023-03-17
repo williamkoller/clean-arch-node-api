b0bb0e8b521f2ea0f7bdc84999a12ac2b03673a2
William Koller <williamkoller404@gmail.com>
2023-03-17 17:08:57 -0300

    feat: add main

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+declare const module: any;
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = app.get<ConfigService>(ConfigService);
+  const appUrl = config.get<number>('APP_URL');
+  const port =   config.get<number>('PORT')
+
+  await app.listen(port, () => logger.log(`Server running at: ${appUrl}`));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
