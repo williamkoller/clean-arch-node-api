4aad490c5ae1753c4be504cd06fc28409dd98812
William Koller <williamkoller404@gmail.com>
2023-03-29 19:07:28 -0300

    feat: :sparkles: add hasher in dependency injection

--- src/main/factories/usecases/user/add-user/add-user.usecase.spec.ts
+++ src/main/factories/usecases/user/add-user/add-user.usecase.spec.ts
@@ -1,3 +1,5 @@
+import { User, UserProps } from '@app/domain/entities/user/user.entity';
+import { BcryptAdapter } from '@app/infra/cryptography/bcrypt-adapter/bcrypt-adapter';
 import { UserDatabaseInMemoryRepository } from '@app/infra/database/repositories/inmemory/user/user.databaseinmemory.repository';
 import { AddUserDTO } from '@app/presentation/dtos/user/add-user/add-user.dto';
 import { ConflictException } from '@nestjs/common';
@@ -6,36 +8,66 @@ import { AddUserUseCase } from './add-user.usecase';
 describe('AddUserUseCase Test', () => {
   it('should be create a new user', async () => {
     const repository = new UserDatabaseInMemoryRepository();
-    const createAccountUseCase = new AddUserUseCase(repository);
+    const bcryptAdapter = new BcryptAdapter();
+    const createAccountUseCase = new AddUserUseCase(repository, bcryptAdapter);
+
+    const userProps: UserProps = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'.toLowerCase(),
+    };
+    const user = User.create(userProps);
+
+    const hash = await bcryptAdapter.hash('any_password');
     const addUserDTO: AddUserDTO = {
       name: 'any_name',
       email: 'any_email@mail.com',
-      password: 'any_password',
+      password: hash,
     };
+
+    jest.spyOn(bcryptAdapter, 'hash').mockResolvedValue(hash);
+
     const output = await createAccountUseCase.create(addUserDTO);
 
     expect(repository.users).toHaveLength(1);
     expect(output).toStrictEqual({
       id: repository.users[0].id,
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+      name: user.props.name,
+      email: user.props.email,
+      password: hash,
     });
   });
 
-  it('should be throw when usecase throw', async () => {
+  fit('should be throw when usecase throw', async () => {
     const repository = new UserDatabaseInMemoryRepository();
-    const createAccountUseCase = new AddUserUseCase(repository);
+    const bcryptAdapter = new BcryptAdapter();
+    const createAccountUseCase = new AddUserUseCase(repository, bcryptAdapter);
+
+    const hash = await bcryptAdapter.hash('any_password');
+
     const addUserDTO: AddUserDTO = {
       name: 'any_name',
       email: 'any_email@mail.com',
-      password: 'any_password',
+      password: hash,
     };
+
+    jest
+      .spyOn(bcryptAdapter, 'hash')
+      .mockImplementation(() => Promise.resolve(hash));
+
     const output = await createAccountUseCase.create(addUserDTO);
 
-    output.email = 'any_email@mail.com';
-    await expect(createAccountUseCase.create(addUserDTO)).rejects.toThrow(
-      new ConflictException(),
-    );
+    const outputConflict = createAccountUseCase.create(addUserDTO);
+
+    expect(output).toStrictEqual({
+      id: output.id,
+      email: 'any_email@mail.com',
+      name: 'any_name',
+      password: hash,
+    });
+
+    jest.spyOn(repository, 'findByEmail');
+    expect(repository.findByEmail).toHaveBeenCalledTimes(0);
+    await expect(outputConflict).rejects.toThrowError(new ConflictException());
   });
 });
--- src/presentation/controllers/user/user.controller.spec.ts
+++ src/presentation/controllers/user/user.controller.spec.ts
@@ -1,4 +1,6 @@
+import { Hasher } from '@app/data/protocols/cryptography/hasher';
 import { UserRepositoryInterface } from '@app/data/protocols/db/user/user.repository.interface';
+import { BcryptAdapter } from '@app/infra/cryptography/bcrypt-adapter/bcrypt-adapter';
 import { UserDatabaseInMemoryRepository } from '@app/infra/database/repositories/inmemory/user/user.databaseinmemory.repository';
 import { AddUserUseCase } from '@app/main/factories/usecases/user/add-user/add-user.usecase';
 import { Test, TestingModule } from '@nestjs/testing';
@@ -7,24 +9,27 @@ import { UserController } from './user.controller';
 describe('UserController', () => {
   let controller: UserController;
   let addUserUseCase: AddUserUseCase;
+  let bcryptAdapter: BcryptAdapter;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [
         UserDatabaseInMemoryRepository,
+        BcryptAdapter,
         {
           provide: AddUserUseCase,
-          useFactory(userRepo: UserRepositoryInterface) {
-            return new AddUserUseCase(userRepo);
+          useFactory(userRepo: UserRepositoryInterface, hasher: Hasher) {
+            return new AddUserUseCase(userRepo, hasher);
           },
-          inject: [UserDatabaseInMemoryRepository],
+          inject: [UserDatabaseInMemoryRepository, BcryptAdapter],
         },
       ],
     }).compile();
 
     controller = moduleFixture.get<UserController>(UserController);
     addUserUseCase = moduleFixture.get<AddUserUseCase>(AddUserUseCase);
+    bcryptAdapter = moduleFixture.get<BcryptAdapter>(BcryptAdapter);
   });
 
   it('should be defined', () => {
@@ -35,18 +40,20 @@ describe('UserController', () => {
     expect(addUserUseCase).toBeDefined();
   });
 
-  it('should ', async () => {
+  it('should be create user in controller', async () => {
+    const hash = await bcryptAdapter.hash('any_password');
+    jest.spyOn(bcryptAdapter, 'hash').mockResolvedValue(hash);
     const response = await controller.create({
       name: 'any_name',
       email: 'any_mail@mail.com',
-      password: 'any_password',
+      password: hash,
     });
 
     expect(response).toEqual({
       id: response.id,
       name: 'any_name',
       email: 'any_mail@mail.com',
-      password: 'any_password',
+      password: hash,
     });
   });
 });
